2dd0cbde369308c56b7100d8d1f1e2b787137ec9
BeeJayTee <91142693+BeeJayTee@users.noreply.github.com>
2022-08-18 16:57:41 -0400

    project complete

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const { default: mongoose } = require('mongoose')
+require('dotenv').config()
+
+const workoutRoutes = require('./routes/workouts')
+
+
+// express app
+const app = express()
+
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+app.use('/api/workouts', workoutRoutes)
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('connected to db')
+        // listen for request
+        app.listen(process.env.PORT || 4000, () => {
+            console.log(`listening on port ${process.env.PORT || 4000}`)
+        }) 
+    })
+    .catch(err => {
+        console.log(err)
+    })
