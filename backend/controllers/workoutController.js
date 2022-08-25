f058868268ac8c88fb3542e977c816be18a42311
BeeJayTee <91142693+BeeJayTee@users.noreply.github.com>
2022-08-25 10:37:04 -0400

    finished

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose')
 
 // get all workouts
 const getWorkouts = async (req, res) => {
-    const workouts = await Workout.find({}).sort({createdAt: -1})
+    const user_id = req.user._id
+
+    const workouts = await Workout.find({ user_id }).sort({createdAt: -1})
 
     res.status(200).json(workouts)
 }
@@ -27,7 +29,6 @@ const getWorkout = async (req, res) => {
 
 // create new workout
 const createWorkout = async (req, res) => {
-    console.log(req.body)
     const {title, load, reps} = req.body
 
     let emptyFields = []
@@ -46,7 +47,8 @@ const createWorkout = async (req, res) => {
     }
 
     try {
-        const workout = await Workout.create({title, load, reps})
+        const user_id = req.user._id
+        const workout = await Workout.create({title, load, reps, user_id})
         res.status(200).json(workout)
     } catch (error) {
         res.status(400).json({ error: error.message })
--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -1,4 +1,7 @@
-const requireAuth = (req, res, next) => {
+const jwt = require('jsonwebtoken')
+const User = require('../models/userModel')
+
+const requireAuth = async (req, res, next) => {
 
     // verify authenitcation
     const { authorization } = req.headers
@@ -7,4 +10,18 @@ const requireAuth = (req, res, next) => {
         return res.status(401).json({error: 'authorization token required'})
     }
 
+    const token = authorization.split(' ')[1]
+
+    try {
+        const {_id} = jwt.verify(token, process.env.SECRET)
+
+        req.user = await User.findOne({ _id }).select('_id')
+        next()
+
+    } catch (error) {
+        console.log(error)
+        res.status(401).json({error: 'request is not authorized'})
+    }
 }
+
+module.exports = requireAuth
